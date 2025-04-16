38ddb58be531d449caa1c76eda7260e1a6772c5a
First-HoWL <154261106+First-HoWL@users.noreply.github.com>
2025-04-16 19:57:14 +0300

    Update Server.cs

--- Class Work 04.16/Server.cs
+++ Class Work 04.16/Server.cs
@@ -1,12 +1,14 @@
 using System.Text;
 using System.Net;
 using System.Net.Sockets;
+using System.Net.Http;
 
 
 class Server
 {
     static TcpListener listener; 
     static int port = 5000;
+    static int clients = 1;
     static void Main(string[] args)
     {
         Console.OutputEncoding = UTF8Encoding.UTF8;
@@ -30,9 +32,20 @@ class Server
     static void HandleClient(object obj)
     {
         TcpClient client = (TcpClient)obj;
-
+        Console.WriteLine();
         Console.WriteLine("New Client");
-        Console.WriteLine(client.Client.RemoteEndPoint.ToString()); 
+
+        var endPoint = client.Client.RemoteEndPoint.ToString();
+        
+
+        NetworkStream stream = client.GetStream();
+        byte[] buffer = new byte[1024];
+        stream.Read(buffer, 0, buffer.Length);
+        
+        string name = Encoding.UTF8.GetString(buffer);
+        Console.WriteLine($"Name {name} | {endPoint.ToString()}");
+        byte[] buffer2 = Encoding.UTF8.GetBytes(Convert.ToString(clients++));
+        stream.Write(buffer2, 0, buffer2.Length);
     }
 
 }
