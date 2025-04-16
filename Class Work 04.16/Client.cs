2686cea429bdb76e7d8e88f3e87015897f71fb6c
First-HoWL <154261106+First-HoWL@users.noreply.github.com>
2025-04-16 19:57:23 +0300

    Update Client.cs

--- Class Work 04.16/Client.cs
+++ Class Work 04.16/Client.cs
@@ -12,8 +12,26 @@ class Client
         Console.OutputEncoding = UTF8Encoding.UTF8;
         Console.InputEncoding = UTF8Encoding.UTF8;
 
+        Console.Write("Type your name: ");
+        string a = Console.ReadLine();
+
+
         TcpClient tcpClient = new TcpClient(serverIP, port);
         Console.WriteLine("Succes!");
+
+
+
+        byte[] buffer = Encoding.UTF8.GetBytes(a);
+        NetworkStream stream = tcpClient.GetStream();
+        stream.Write(buffer, 0, buffer.Length);
+
+        NetworkStream stream2 = tcpClient.GetStream();
+        byte[] buffer2 = new byte[1024];
+        stream2.Read(buffer2, 0, buffer2.Length);
+
+        string number = Encoding.UTF8.GetString(buffer2);
+        Console.WriteLine($"You are {Convert.ToInt32(number)} client");
+
         Console.ReadLine();
     }
 }
